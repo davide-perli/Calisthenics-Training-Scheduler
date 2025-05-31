bbdac35b8dbbe6b1cd23a9bf383d94576d0b64d4
davide-perli <perlidavide@gmail.com>
2025-06-01 00:17:18 +0300

    showing+deleting database

--- CalisthenicsApp.java
+++ CalisthenicsApp.java
@@ -1,30 +1,90 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class CalisthenicsApp {
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         String name_input;
+        boolean alreadyExists = false;
+        Service_class service = new Service_class();
         System.out.println("\nWelcome to my calisthenics app where you can create your personalized training program!\n");
         while (true) {
             System.out.println("Enter your name: ");
             name_input = myObj.nextLine();
-            
-            if (name_input.matches("[a-zA-Z]+")) {
-                break;
-            } else {
-                System.out.println("Invalid name! Please enter only letters.");
+
+            if (!name_input.matches("[a-zA-Z]+")) {
+                System.out.println("Invalid name! Please enter only letters!\n");
+                continue;
+            }
+
+            String safeName = name_input.toLowerCase().replaceAll("[^a-z]", "");
+            String tableName = safeName + "_training_plan";
+
+            try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/calisthenics_training_program", "postgres", "dlmvm")) {
+                if (service.doesTableExist(connection, tableName)) {
+                    alreadyExists = true;
+                    break;
+                } else {
+                    break; 
+                }
+            } catch (SQLException e) {
+                System.err.println("Error: " + e.getMessage());
             }
         }
 
         Athlete user = new Athlete();
         user.setName(name_input);
 
-        System.out.println("\n\nWelcome " + user.getName()+ "!\nCreate you training program for your skill level:\n");
+        if (alreadyExists) {
+            System.out.println("\nWelcome back " + user.getName() + "!\n\nChoose what you want to do:\n1. Create a new training program\n2. View existing training program\n3. Delete training program\n");
+            int option;
+            while (true) {
+                System.out.print("Option: ");
+                option = myObj.nextInt();
+                
+                if (option >= 1 && option <= 3) {
+                    break;
+                } else {
+                    System.out.println("Invalid option! Please enter a number between 1 and 3 as your option!\n");
+                }
+            }
 
-        Training training = new Training(user);
-        training.putInDatabase(training.getWeeklyPlan());
-        System.out.println("Athlete " + user.getName() + " training program: ");
-        training.showPlan(training.getWeeklyPlan());
+            if (option == 1) {
+                System.out.println("\nYou chose to create a new training program.\n");
+                Training training = new Training(user);
+                training.putInDatabase(training.getWeeklyPlan());
+                System.out.println("Athlete " + user.getName() + " training program: ");
+                training.showPlan(training.getWeeklyPlan());
+            } else if (option == 2) {
+                System.out.println("\nYou chose to view your existing training program.\n");
+                service.showExistingPlan(user.getName());
+            } else {
+                System.out.println("\nAre you sure you want to delete your training program?(YES or NO)\nWARNING: This action cannot be undone!\n");
+                myObj.nextLine(); // Consume the newline character
+                String confirmation = myObj.nextLine().trim().toUpperCase();
+                while (!confirmation.equals("YES") && !confirmation.equals("NO") && !confirmation.equals("Y") && !confirmation.equals("N")) {
+                    System.out.println("Invalid input! Please type 'YES' or 'NO'.");
+                    confirmation = myObj.nextLine().trim().toUpperCase();
+                }
+                
+                if (confirmation.equals("NO") || confirmation.equals("N")) {
+                    System.out.println("\nYou chose not to delete your training program.\n");
+                } else if (confirmation.equals("YES") || confirmation.equals("Y")) {
+                    System.out.println("\nYou chose to delete your training program.\n");
+                    service.deletePlan(user.getName());
+                }
+            }
+        } else {
+        
+            System.out.println("\n\nWelcome " + user.getName()+ "!\nCreate you training program for your skill level:\n");
+
+            Training training = new Training(user);
+            training.putInDatabase(training.getWeeklyPlan());
+            System.out.println("Athlete " + user.getName() + " training program: ");
+            training.showPlan(training.getWeeklyPlan());
+        }
 
         myObj.close();
     }
--- Service_class.java
+++ Service_class.java
@@ -33,22 +33,40 @@ public class Service_class extends Exercise {
         }
     }
 
+    public boolean doesTableExist(Connection connection, String tableName) throws SQLException {
+        String checkTableSQL = "SELECT EXISTS (" +
+                "SELECT FROM information_schema.tables " +
+                "WHERE table_schema = 'public' AND table_name = ?" +
+                ")";
+        try (PreparedStatement pstmt = connection.prepareStatement(checkTableSQL)) {
+            pstmt.setString(1, tableName);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return rs.getBoolean(1);
+                }
+            }
+        }
+        return false;
+    }
+
     public void storePlan(String athleteName, HashMap<String, ExercisePlan> weeklyPlan) {
         String url = "jdbc:postgresql://localhost:5432/calisthenics_training_program";
         String user = "postgres";
         String password = "dlmvm";
 
-        String createTableSQL = "CREATE TABLE IF NOT EXISTS training_plan (" +
+
+        String lower_case_AthleteName = athleteName.toLowerCase().replaceAll("[^a-z]", "");
+        String tableName = lower_case_AthleteName + "_training_plan";
+
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS " + tableName + " (" +
                 "id SERIAL PRIMARY KEY, " +
-                "athlete_name VARCHAR(50) NOT NULL, " +
                 "day VARCHAR(10) NOT NULL, " +
                 "static_exercise VARCHAR(100) NOT NULL, " +
                 "dynamic_exercise VARCHAR(100) NOT NULL, " +
                 "sets_reps_exercise VARCHAR(100) NOT NULL" +
                 ")";
 
-        String insertSQL = "INSERT INTO training_plan (athlete_name, day, static_exercise, dynamic_exercise, sets_reps_exercise) " +
-                "VALUES (?, ?, ?, ?, ?)";
+        String insertSQL = "INSERT INTO " + tableName + " (day, static_exercise, dynamic_exercise, sets_reps_exercise) " + "VALUES (?, ?, ?, ?)";
 
         try {
             Class.forName("org.postgresql.Driver");
@@ -60,20 +78,20 @@ public class Service_class extends Exercise {
                 System.out.println("Connected to PostgreSQL database successfully!");
 
                 statement.execute(createTableSQL);
-                System.out.println("Table 'training_plan' created/verified successfully!");
+                System.out.println("Table '" + tableName + "' created or verified successfully!");
 
-                try (PreparedStatement pstmt = connection.prepareStatement(insertSQL)) {
+                try (PreparedStatement insert = connection.prepareStatement(insertSQL)) {
                     for (Map.Entry<String, ExercisePlan> entry : weeklyPlan.entrySet()) {
                         String day = entry.getKey();
                         ExercisePlan plan = entry.getValue();
 
-                        pstmt.setString(1, athleteName);
-                        pstmt.setString(2, day);
-                        pstmt.setString(3, plan.getStaticExercise());
-                        pstmt.setString(4, plan.getDynamicExercise());
-                        pstmt.setString(5, plan.getSetsAndReps());
 
-                        pstmt.executeUpdate();
+                        insert.setString(1, day);
+                        insert.setString(2, plan.getStaticExercise());
+                        insert.setString(3, plan.getDynamicExercise());
+                        insert.setString(4, plan.getSetsAndReps());
+
+                        insert.executeUpdate();
                     }
                 }
 
@@ -85,6 +103,60 @@ public class Service_class extends Exercise {
             e.printStackTrace();
         } catch (SQLException e) {
             System.err.println("Database error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void deletePlan(String athleteName) {
+        String url = "jdbc:postgresql://localhost:5432/calisthenics_training_program";
+        String user = "postgres";
+        String password = "dlmvm";
+
+        String tableName = athleteName.toLowerCase().replaceAll("[^a-z]", "") + "_training_plan";
+        String dropTableSQL = "DROP TABLE IF EXISTS " + tableName;
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             Statement statement = connection.createStatement()) {
+
+            statement.execute(dropTableSQL);
+            System.out.println("Training program for athlete '" + athleteName + "' deleted successfully.");
+
+        } catch (SQLException e) {
+            System.err.println("Error deleting training program: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public void showExistingPlan(String athleteName) {
+        String url = "jdbc:postgresql://localhost:5432/calisthenics_training_program";
+        String user = "postgres";
+        String password = "dlmvm";
+
+        String tableName = athleteName.toLowerCase().replaceAll("[^a-z]", "") + "_training_plan";
+        String selectSQL = "SELECT day, static_exercise, dynamic_exercise, sets_reps_exercise " +
+                        "FROM " + tableName;
+
+        HashMap<String, ExercisePlan> weeklyPlan = new HashMap<>();
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(selectSQL)) {
+
+            while (rs.next()) {
+                String day = rs.getString("day");
+                String staticEx = rs.getString("static_exercise");
+                String dynamicEx = rs.getString("dynamic_exercise");
+                String setsReps = rs.getString("sets_reps_exercise");
+
+                ExercisePlan plan = new ExercisePlan(staticEx, dynamicEx, setsReps);
+                weeklyPlan.put(day, plan);
+            }
+
+            Map<String, ExercisePlan> sortedPlan = sortMap(weeklyPlan);
+            outPlan(weeklyPlan, sortedPlan);
+
+        } catch (SQLException e) {
+            System.err.println("Error fetching training program: " + e.getMessage());
             e.printStackTrace();
         }
     }
